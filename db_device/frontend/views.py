03b9f7d1061c096ea992985348300826705a17c5
Alexey Inkov <alex0982@mail.ru>
2024-11-06 00:58:08 +0300

    add test view

--- db_device/frontend/views.py
+++ db_device/frontend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+    return render(request, 'frontend/index.html', context=data)
+
+
+def show_organization(request, org_id):
+    data = {
+        'title': 'Отображение по организации',
+        'org_selected': org_id,
+    }
+    return render(request, 'frontend/index.html', context=data)
