05881f5a4764a6a59bb8f49ec725567371b97c44
Alexey Inkov <alex0982@mail.ru>
2024-11-09 02:38:21 +0300

    add class IndexView

--- db_device/frontend/views.py
+++ db_device/frontend/views.py
@@ -1,79 +1,56 @@
 from django.contrib.auth.views import LoginView
+from django.db.models import Q
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, TemplateView
 
+from device.models import Device
+from metering_unit.models import Organization, UserToOrganization, MeteringUnit
 from .forms import LoginUserForm, RegisterUserForm
 from .mixins import DataMixin
 
 
 class LoginUserView(LoginView):
     form_class = LoginUserForm
-    template_name = 'frontend/login.html'
-    extra_context = {'title': 'Авторизация'}
+    template_name = "frontend/login.html"
+    extra_context = {"title": "Авторизация"}
 
 
 class RegisterUserView(CreateView):
     form_class = RegisterUserForm
-    template_name = 'frontend/register.html'
-    extra_context = {'title': "Регистрация"}
-    success_url = reverse_lazy('frontend:login')
+    template_name = "frontend/register.html"
+    extra_context = {"title": "Регистрация"}
+    success_url = reverse_lazy("frontend:login")
 
 
-class IndexView(DataMixin, ListView):
-    template_name = 'frontend/index.html'
-    context_object_name = 'posts'
-    title_page = 'Главная страница'
-    tso_selected = 0
+class IndexView(DataMixin, TemplateView):
+    template_name = "frontend/index.html"
+    title_page = "Главная страница"
 
-    def get_queryset(self):
-        return Women.published.all().select_related('cat')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
+        context["orgs"] = Organization.objects.filter(
+            user_to_org__user=self.request.user
+        )
+        context["metering_units"] = (
+            MeteringUnit.objects.filter(
+                Q(tso__in=context["orgs"])
+                | Q(customer__in=context["orgs"])
+                | Q(service_organization__in=context["orgs"])
+            )
+            .select_related("address__region")
+            .select_related("address__street__type_street")
+        )
 
-def index(request):
-    data = {
-        'title': 'Главная страница',
-        'tso_selected': 0,
-    }
-    return render(request, 'frontend/index.html', context=data)
+        context["devices"] = Device.objects.filter(
+            metering_unit__in=context["metering_units"].values_list("pk", flat=True)
+        ).select_related("type_of_file")
 
+        context["tso_selected"] = self.request.GET.get("tso", "all")
+        context["cust_selected"] = self.request.GET.get("customer", "all")
+        context["mu_selected"] = self.request.GET.get("metering_unit", "all")
+        context["dev_selected"] = self.request.GET.get("device", "all")
+        # context["verif"] = context["devices"].filter(pk=context["dev_selected"])
 
-def show_tso(request, tso_id):
-    data = {
-        'title': 'Отображение по ТСО',
-        'tso_selected': tso_id,
-        'cust_selected': 0,
-    }
-    return render(request, 'frontend/index.html', context=data)
-
-
-def show_customers(request, tso_id, customer_id):
-    data = {
-        'title': 'Отображение по Абоненту',
-        'tso_selected': tso_id,
-        'cust_selected': customer_id,
-        'mu_selected': 0,
-    }
-    return render(request, 'frontend/index.html', context=data)
-
-
-def show_metering_units(request, tso_id, customer_id, mu_id):
-    data = {
-        'title': 'Отображение по УУТЭ',
-        'tso_selected': tso_id,
-        'cust_selected': customer_id,
-        'mu_selected': mu_id,
-        'dev_selected': 0,
-    }
-    return render(request, 'frontend/index.html', context=data)
-
-
-def show_devices(request, tso_id, customer_id, mu_id, dev_id):
-    data = {
-        'title': 'Отображение по УУТЭ',
-        'tso_selected': tso_id,
-        'cust_selected': customer_id,
-        'mu_selected': mu_id,
-        'dev_selected': dev_id,
-    }
-    return render(request, 'frontend/index.html', context=data)
+        return context
