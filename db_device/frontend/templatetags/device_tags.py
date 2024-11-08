b1c460fba488f2e15431667dc4f2025e68723bee
Alexey Inkov <alex0982@mail.ru>
2024-11-09 02:40:12 +0300

    fix temp tags

--- db_device/frontend/templatetags/device_tags.py
+++ db_device/frontend/templatetags/device_tags.py
@@ -1,60 +1,60 @@
 from django import template
 
-from device.models import Device
-from metering_unit.models import MeteringUnit, Organization
 
 register = template.Library()
 
 
-@register.inclusion_tag('frontend/list_tso.html')
-def show_tso(tso_selected):
-    tso_s = Organization.objects.filter(pk__in=(
-        MeteringUnit.objects
-        .values("tso")
-        .distinct()
-    ))
-    return {'tso_s': tso_s, 'tso_selected': tso_selected}
-
-
-@register.inclusion_tag('frontend/list_customers.html')
-def show_customers(tso_selected, cust_selected):
+@register.inclusion_tag("frontend/list_menu.html")
+def show_menu(orgs, metering_units, tso_selected, cust_selected):
+    tso_s = orgs.filter(pk__in=(metering_units.values("tso").distinct()))
     filters = {}
-    if tso_selected != 0:
-        filters['tso'] = tso_selected
-    customers = Organization.objects.filter(pk__in=(
-        MeteringUnit.objects.filter(**filters)
-        .values("customer")
-        .distinct()
-    ))
-    return {'customers': customers, 'tso_selected': tso_selected, 'cust_selected': cust_selected}
+    if tso_selected != "all":
+        filters["tso"] = tso_selected
+    customers = orgs.filter(
+        id__in=(metering_units.filter(**filters).values("customer").distinct())
+    )
+    return {
+        "tso_s": tso_s,
+        "customers": customers,
+        "tso_selected": tso_selected,
+        "cust_selected": cust_selected,
+    }
 
 
-@register.inclusion_tag('frontend/list_metering_units.html')
-def show_metering_units(tso_selected, cust_selected, mu_selected):
+@register.inclusion_tag("frontend/list_metering_units.html")
+def show_metering_units(metering_units, tso_selected, cust_selected, mu_selected):
     filters = {}
-    if tso_selected != 0:
-        filters['tso'] = tso_selected
-    if cust_selected == '':
-        cust_selected = 0
-    if cust_selected != 0:
-        filters['customer'] = cust_selected
+    if tso_selected != "all":
+        filters["tso"] = tso_selected
+    if cust_selected != "all":
+        filters["customer"] = cust_selected
 
-    metering_units = (MeteringUnit.objects.filter(**filters)
-                      .select_related('address__region')
-                      .select_related('address__street__type_street')
-                      )
+    metering_units = (
+        metering_units.filter(**filters)
+        .select_related("address__region")
+        .select_related("address__street__type_street")
+    )
 
-    return {'metering_units': metering_units, 'tso_selected': tso_selected, 'cust_selected': cust_selected, 'mu_selected': mu_selected}
+    return {
+        "metering_units": metering_units,
+        "tso_selected": tso_selected,
+        "cust_selected": cust_selected,
+        "mu_selected": mu_selected,
+    }
 
 
-@register.inclusion_tag('frontend/list_devices.html')
-def show_devices(tso_selected, cust_selected, mu_selected, dev_selected):
+@register.inclusion_tag("frontend/list_devices.html")
+def show_devices(devices, tso_selected, cust_selected, mu_selected, dev_selected):
     filters = {}
-    if mu_selected == '':
-        mu_selected = 0
-    if mu_selected != 0:
-        filters['metering_unit'] = mu_selected
+    if mu_selected != "all":
+        filters["metering_unit"] = mu_selected
 
-    devices = Device.objects.filter(metering_unit=mu_selected).select_related('type_of_file')
+    devices = devices.filter(**filters)
 
-    return {'devices': devices, 'tso_selected': tso_selected, 'cust_selected': cust_selected, 'mu_selected': mu_selected, 'dev_selected': dev_selected}
+    return {
+        "devices": devices,
+        "tso_selected": tso_selected,
+        "cust_selected": cust_selected,
+        "mu_selected": mu_selected,
+        "dev_selected": dev_selected,
+    }
