d8f54968c0823063e98e3565c45c3331c2cd3323
Alexey Inkov <alex0982@mail.ru>
2024-11-19 02:09:00 +0300

    optimization Queryset

--- db_device/frontend/views.py
+++ db_device/frontend/views.py
@@ -4,8 +4,7 @@ from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
-from config.settings import LOGIN_URL
-from device.models import Device, DeviceVerification
+from device.models import Device
 from metering_unit.models import Organization, MeteringUnit
 from .forms import LoginUserForm, RegisterUserForm
 from .mixins import DataMixin
@@ -38,11 +37,26 @@ class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["orgs"] = Organization.objects.filter(
+        context["orgs"] = Organization.objects.only("name").filter(
             user_to_org__user=self.request.user
         )
         context["metering_units"] = (
-            MeteringUnit.objects.filter(
+            MeteringUnit.objects.only(
+                "customer__name",
+                "address__region__name",
+                "address__region__parent_region__name",
+                "address__street__type_street__name",
+                "address__street__name",
+                "address__house_number",
+                "address__corp",
+                "address__liter",
+                "address__latitude",
+                "address__longitude",
+                "itp",
+                "tso__name",
+                "service_organization__name",
+            )
+            .filter(
                 Q(tso__in=context["orgs"])
                 | Q(customer__in=context["orgs"])
                 | Q(service_organization__in=context["orgs"])
@@ -54,16 +68,25 @@ class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
         )
 
         context["devices"] = (
-            Device.objects.filter(
+            Device.objects.only(
+                "installation_point__name",
+                "registry_number__registry_number",
+                "type__type",
+                "mod__mod",
+                "type_of_file__device_type_file",
+                "type_of_file__numbers_registry",
+                "factory_number",
+                "notes",
+            )
+            .filter(
                 metering_unit__in=context["metering_units"].values_list("pk", flat=True)
             )
             .select_related("type_of_file")
             .select_related("installation_point")
+            .prefetch_related("verifications")
         )
 
-        context["verifications"] = DeviceVerification.objects.filter(
-            device__in=context["devices"].values_list("pk", flat=True)
-        )
+        # context["verif"] = DeviceVerification.objects.all()
 
         context["tso_selected"] = self.request.GET.get("tso", "all")
         context["cust_selected"] = self.request.GET.get("customer", "all")
