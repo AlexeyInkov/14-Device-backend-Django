0af3852cb4ff688d639ce13e6cb06d0bbe38a855
Alexey Inkov <alex0982@mail.ru>
2024-11-25 20:44:45 +0300

    fix view

--- db_device/frontend/views.py
+++ db_device/frontend/views.py
@@ -12,13 +12,13 @@ from .mixins import DataMixin
 
 class LoginUserView(LoginView):
     form_class = LoginUserForm
-    template_name = "frontend/login.html"
+    template_name = "frontend/auth/login.html"
     extra_context = {"title": "Авторизация"}
 
 
 class RegisterUserView(CreateView):
     form_class = RegisterUserForm
-    template_name = "frontend/register.html"
+    template_name = "frontend/auth/register.html"
     extra_context = {"title": "Регистрация"}
     success_url = reverse_lazy("frontend:login")
 
@@ -31,15 +31,27 @@ class MyLogoutView(LogoutView):
 
 
 class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
-    template_name = "frontend/index.html"
+    template_name = "frontend/index/index.html"
     title_page = "Главная страница"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        context["org_selected"] = self.request.GET.get("org-select", "all")
+        context["tso_selected"] = self.request.GET.get("tso", "all")
+        context["cust_selected"] = self.request.GET.get("customer", "all")
+        context["mu_selected"] = self.request.GET.get("metering_unit", "all")
+        context["dev_selected"] = self.request.GET.get("device", "all")
+        context["verif_selected"] = self.request.GET.get("verification", "all")
+
+        # Orgs query
         context["orgs"] = Organization.objects.only("name").filter(
             user_to_org__user=self.request.user
         )
+        # Orgs filter
+        if context["org_selected"] != "all":
+            context["orgs"] = context["orgs"].filter(pk=context["org_selected"])
+        # MeteringUnit query
         context["metering_units"] = (
             MeteringUnit.objects.only(
                 "customer__name",
@@ -56,17 +68,42 @@ class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
                 "tso__name",
                 "service_organization__name",
             )
-            .filter(
-                Q(tso__in=context["orgs"])
-                | Q(customer__in=context["orgs"])
-                | Q(service_organization__in=context["orgs"])
-            )
             .select_related("address__region")
             .select_related("address__street__type_street")
             .select_related("customer")
             .select_related("tso")
         )
+        # MeteringUnit filter
+        filters = {}
+        if context["tso_selected"] != "all":
+            filters["tso"] = context["tso_selected"]
+        if context["cust_selected"] != "all":
+            filters["customer"] = context["cust_selected"]
 
+        context["metering_units"] = context["metering_units"].filter(
+            Q(tso__in=context["orgs"])
+            | Q(customer__in=context["orgs"])
+            | Q(service_organization__in=context["orgs"])
+        )
+        if filters:
+            context["metering_units"] = context["metering_units"].filter(**filters)
+        # TSO filter
+        context["tso_s"] = context["orgs"].filter(
+            pk__in=(context["metering_units"].values("tso").distinct())
+        )
+        filters = {}
+        if context["tso_selected"] != "all":
+            filters["tso"] = context["tso_selected"]
+        # Customer filter
+        context["customers"] = context["orgs"].filter(
+            id__in=(
+                context["metering_units"]
+                .filter(**filters)
+                .values("customer")
+                .distinct()
+            )
+        )
+        # Device query
         context["devices"] = (
             Device.objects.only(
                 "installation_point__name",
@@ -78,20 +115,18 @@ class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
                 "factory_number",
                 "notes",
             )
-            .filter(
-                metering_unit__in=context["metering_units"].values_list("pk", flat=True)
-            )
             .select_related("type_of_file")
             .select_related("installation_point")
             .prefetch_related("verifications")
         )
-
-        # context["verif"] = DeviceVerification.objects.all()
-
-        context["tso_selected"] = self.request.GET.get("tso", "all")
-        context["cust_selected"] = self.request.GET.get("customer", "all")
-        context["mu_selected"] = self.request.GET.get("metering_unit", "all")
-        context["dev_selected"] = self.request.GET.get("device", "all")
-        context["verif_selected"] = self.request.GET.get("verification", "all")
+        # Device filter
+        if context["mu_selected"] and context["mu_selected"] != "all":
+            context["devices"] = context["devices"].filter(
+                metering_unit__in=[context["mu_selected"]]
+            )
+        else:
+            context["devices"] = context["devices"].filter(
+                metering_unit__in=context["metering_units"].values_list("pk", flat=True)
+            )
 
         return context
