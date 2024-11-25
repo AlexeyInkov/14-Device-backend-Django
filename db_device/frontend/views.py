386bffd3ba32653e555183223fb0be49c0916a8a
Alexey Inkov <alex0982@mail.ru>
2024-11-25 21:59:00 +0300

    fix get contex data

--- db_device/frontend/views.py
+++ db_device/frontend/views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
-from device.models import Device
-from metering_unit.models import Organization, MeteringUnit
 from .forms import LoginUserForm, RegisterUserForm
 from .mixins import DataMixin
+from .services import get_organizations, get_metering_units, get_devices
 
 
 class LoginUserView(LoginView):
@@ -37,96 +35,40 @@ class IndexView(DataMixin, LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["org_selected"] = self.request.GET.get("org-select", "all")
-        context["tso_selected"] = self.request.GET.get("tso", "all")
-        context["cust_selected"] = self.request.GET.get("customer", "all")
-        context["mu_selected"] = self.request.GET.get("metering_unit", "all")
-        context["dev_selected"] = self.request.GET.get("device", "all")
-        context["verif_selected"] = self.request.GET.get("verification", "all")
+        org_selected = self.request.GET.get("org_select", "all")
+        tso_selected = self.request.GET.get("tso", "all")
+        cust_selected = self.request.GET.get("customer", "all")
+        mu_selected = self.request.GET.get("metering_unit", "all")
+        dev_selected = self.request.GET.get("device", "all")
 
-        # Orgs query
-        context["orgs"] = Organization.objects.only("name").filter(
-            user_to_org__user=self.request.user
-        )
-        # Orgs filter
-        if context["org_selected"] != "all":
-            context["orgs"] = context["orgs"].filter(pk=context["org_selected"])
-        # MeteringUnit query
-        context["metering_units"] = (
-            MeteringUnit.objects.only(
-                "customer__name",
-                "address__region__name",
-                "address__region__parent_region__name",
-                "address__street__type_street__name",
-                "address__street__name",
-                "address__house_number",
-                "address__corp",
-                "address__liter",
-                "address__latitude",
-                "address__longitude",
-                "itp",
-                "tso__name",
-                "service_organization__name",
-            )
-            .select_related("address__region")
-            .select_related("address__street__type_street")
-            .select_related("customer")
-            .select_related("tso")
-        )
-        # MeteringUnit filter
-        filters = {}
-        if context["tso_selected"] != "all":
-            filters["tso"] = context["tso_selected"]
-        if context["cust_selected"] != "all":
-            filters["customer"] = context["cust_selected"]
+        orgs = get_organizations(org_selected, self.request.user)
 
-        context["metering_units"] = context["metering_units"].filter(
-            Q(tso__in=context["orgs"])
-            | Q(customer__in=context["orgs"])
-            | Q(service_organization__in=context["orgs"])
+        filter_metering_units, metering_units = get_metering_units(
+            tso_selected, cust_selected, orgs
         )
-        if filters:
-            context["metering_units"] = context["metering_units"].filter(**filters)
-        # TSO filter
-        context["tso_s"] = context["orgs"].filter(
-            pk__in=(context["metering_units"].values("tso").distinct())
-        )
+
+        devices = get_devices(mu_selected, filter_metering_units)
+
+        tso_s = orgs.filter(pk__in=(metering_units.values("tso").distinct()))
+
         filters = {}
-        if context["tso_selected"] != "all":
-            filters["tso"] = context["tso_selected"]
-        # Customer filter
-        context["customers"] = context["orgs"].filter(
-            id__in=(
-                context["metering_units"]
-                .filter(**filters)
-                .values("customer")
-                .distinct()
-            )
+        if tso_selected != "all":
+            filters["tso"] = tso_selected
+        customers = orgs.filter(
+            id__in=(metering_units.filter(**filters).values("customer").distinct())
         )
-        # Device query
-        context["devices"] = (
-            Device.objects.only(
-                "installation_point__name",
-                "registry_number__registry_number",
-                "type__type",
-                "mod__mod",
-                "type_of_file__device_type_file",
-                "type_of_file__numbers_registry",
-                "factory_number",
-                "notes",
-            )
-            .select_related("type_of_file")
-            .select_related("installation_point")
-            .prefetch_related("verifications")
-        )
-        # Device filter
-        if context["mu_selected"] and context["mu_selected"] != "all":
-            context["devices"] = context["devices"].filter(
-                metering_unit__in=[context["mu_selected"]]
-            )
-        else:
-            context["devices"] = context["devices"].filter(
-                metering_unit__in=context["metering_units"].values_list("pk", flat=True)
-            )
+
+        context = {
+            "orgs": orgs,
+            "tso_s": tso_s,
+            "customers": customers,
+            "metering_units": filter_metering_units,
+            "devices": devices,
+            "org_selected": org_selected,
+            "tso_selected": tso_selected,
+            "cust_selected": cust_selected,
+            "mu_selected": mu_selected,
+            "dev_selected": dev_selected,
+        }
 
         return context
