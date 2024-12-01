f5d7065cb1bddccc3f54158b97dcbd473d146675
Alexey Inkov <alex0982@mail.ru>
2024-12-01 21:22:21 +0300

    fix import

--- core/config/urls.py
+++ core/config/urls.py
@@ -1,12 +1,11 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
-
-from . import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,20 +19,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path(
-            "docs/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-        path("", include("frontend.urls")),
-        path("auth/", include("my_auth.urls")),
-        path("v1/page/", include("for_page.urls")),
-        path("v1/metering_unit/", include("metering_unit.urls")),
-        path("v1/device/", include("device.urls")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + debug_toolbar_urls()
+        [
+            path("admin/", admin.site.urls),
+            path(
+                "docs/",
+                schema_view.with_ui("swagger", cache_timeout=0),
+                name="schema-swagger-ui",
+            ),
+            path("", include("frontend.urls")),
+            path("auth/", include("my_auth.urls")),
+            path("v1/page/", include("for_page.urls")),
+            path("v1/metering_unit/", include("metering_unit.urls")),
+            path("v1/device/", include("device.urls")),
+        ]
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + debug_toolbar_urls()
 )
