72f666cc22b803267048ef471aa5b7b860546969
Alexey Inkov <alex0982@mail.ru>
2024-12-01 21:20:16 +0300

    fix import

--- core/frontend/arshin_servises.py
+++ core/frontend/arshin_servises.py
@@ -2,11 +2,11 @@ from time import sleep
 
 from requests import Session, Response, request
 
-from config.settings import TIME_DDOS_FOR_REQUEST
+from django.conf import settings
 
 
 def request_to_arshin(reg_number: str, number: str) -> Response:
     url = f"https://fgis.gost.ru/fundmetrology/cm/xcdb/vri/select?fq=mi.mitnumber:{reg_number}*&fq=mi.number:{number}&q=*&fl=org_title,mi.mitnumber,mi.mititle,mi.mitype,mi.modification,mi.number,verification_date,valid_date&sort=verification_date+desc,org_title+asc&rows=200&start=0"
-    sleep(TIME_DDOS_FOR_REQUEST)
-    response = request('get', url, timeout=2)
+    sleep(settings.TIME_DDOS_FOR_REQUEST)
+    response = request('get', url)
     return response
