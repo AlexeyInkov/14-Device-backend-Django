9da11f353e3970d85fbff31d940b7f381c26cbe9
Alexey Inkov <alex0982@mail.ru>
2024-12-01 21:22:02 +0300

    fix task add return

--- core/frontend/tasks.py
+++ core/frontend/tasks.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from celery import shared_task
 
 from device.models import Device
@@ -8,34 +6,29 @@ from frontend.db_services import save_verification
 
 
 @shared_task(name='tasks.get_device_verifications')
-def get_device_verifications(device_id: int) -> None:
+def get_device_verifications(device_id: int) -> str:
     print("run get_device_verifications")
     device = Device.objects.get(id=device_id)
-    # print(device)
-    # session = requests.Session()
     if device:
-        # print("numbers_registry=", device.type_of_file.numbers_registry.split(","))
         for reg_number in device.type_of_file.numbers_registry.split(","):
-            # print(reg_number, device.factory_number)
-            response = request_to_arshin(
-                reg_number, device.factory_number
-            )
-            response = response.json()["response"]
-            if response['numFound'] > 0:
-                verifications = response["docs"]
-                # print("verifications=", verifications)
-                if verifications:
-                    for verification in verifications:
-                        # print("verification=", verification)
-                        save_verification(device_id, verification)
+            response = request_to_arshin(reg_number, device.factory_number)
+            if response.status_code == 200:
+                response = response.json()["response"]
+                if response['numFound'] > 0:
+                    verifications = response["docs"]
+                    if verifications:
+                        for verification in verifications:
+                            save_verification(device_id, verification)
+        return "Done"
+    return "Device not found"
 
 
 @shared_task(name='tasks.refresh_valid_date')
-def refresh_valid_date() -> None:
+def refresh_valid_date() -> str:
     print("run refresh_valid_date")
     devices = Device.objects.all().order_by("updated_at").only("id")
     # print("devices_id=", devices)
     for index, device in enumerate(devices):
         print("index=", index)
-        get_device_verifications(device.pk)
-
+        get_device_verifications.delay(device.pk)
+    return "Done"
