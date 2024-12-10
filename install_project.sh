971a85d50393c224b07f90d09423e92c8e73176c
Alexey Inkov <alex0982@mail.ru>
2024-12-10 23:24:41 +0300

    add install_project.sh

---
+++ install_project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+base_python_interpreter=/home/www/.python/bin/python3.11
+project_domain=inkov.online
+project_path=`pwd`
+
+#read -p "Python interpreter: " base_python_interpreter
+#read -p "Your domain without protocol (for example, google.com): " project_domain
+
+sudo certbot --nginx -d $project_domain
+
+`$base_python_interpreter -m venv env`
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+
+sed -i "s~dbms_template_path~$project_path~g" nginx/site.conf etc/systemd/gunicorn.service
+sed -i "s~dbms_template_domain~$project_domain~g" nginx/site.conf src/config/settings.py
+
+sudo ln -s $project_path/etc/nginx/site.conf /etc/nginx/sites-enabled/
+sudo ln -s $project_path/etc/systemd/gunicorn.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+sudo service nginx restart
