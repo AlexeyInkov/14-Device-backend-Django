e5d7ec10ac7266819e318aa72ad87573a2b08709
Alexey Inkov <alex0982@mail.ru>
2024-12-10 23:45:00 +0300

    fix install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -13,11 +13,12 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-sed -i "s~dbms_template_path~$project_path~g" nginx/site.conf etc/systemd/gunicorn.service
-sed -i "s~dbms_template_domain~$project_domain~g" nginx/site.conf src/config/settings.py
+sed -i "s~dbms_template_path~$project_path~g" etc/nginx/site_dc.conf etc/systemd/gunicorn_dc.service
+sed -i "s~dbms_template_domain~$project_domain~g" etc/nginx/site_dc.conf src/config/settings.py
 
-sudo ln -s $project_path/etc/nginx/site.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/etc/systemd/gunicorn.service /etc/systemd/system/
+sudo ln -s $project_path/etc/nginx/site_dc.conf /etc/nginx/sites-enabled/
+sudo ln -s $project_path/etc/systemd/gunicorn_dc.service /etc/systemd/system/
+sudo ln -s $project_path/etc/systemd/gunicorn_dc.socket /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
