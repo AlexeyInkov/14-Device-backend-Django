91eb9bb9698118c377ed21647623beca860c8122
Alexey Inkov <alex0982@mail.ru>
2024-12-12 23:09:25 +0300

    fix install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -1,29 +1,60 @@
 #!/bin/bash
 base_python_interpreter=/home/www/.python/bin/python3.11
 project_domain=inkov.online
+gunicorn_name=dc
 project_path=`pwd`
 
+sudo apt update && sudo apt upgrade -y
+sudo apt install certbot python3-certbot-nginx
 
+echo -n "Удаляем настройки nginx? : "
+read choice
+if [choice=="y"]; then
+  echo "Удаляем настройки nginx"
+  sudo rm -r /etc/nginx/sites-enabled/*
+fi
+echo "Добавляем переадресацию 80 -> 443 в настройки nginx"
+echo "server { listen 80; return 301 https://$host$request_uri; }" > /etc/nginx/sites-enabled/default
+
+echo "restart nginx"
+sudo nginx -t
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+
+echo "Проверяем certificates."
+if [ ! -d "/etc/letsencrypt/live/$project_domain" ]; then
+  echo "certificate does not exist."
+  sudo certbot --nginx -d $project_domain
+fi
+
+echo "Устанавливаем окружение и зависимости."
 `$base_python_interpreter -m venv env`
 source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 python -V
 
-sed -i "s~dbms_template_path~$project_path~g" etc/nginx/site_dc.conf etc/systemd/gunicorn.service
-sed -i "s~dbms_template_domain~$project_domain~g" etc/nginx/site_dc.conf core/config/settings.py
+echo "Подготовка настроек Nginx и Gunicorn"
+sed -i "s~dbms_template_path~$project_path~g" etc/nginx/my_site.conf etc/systemd/gunicorn.service
+sed -i "s~dbms_template_domain~$project_domain~g" etc/nginx/my_site.conf core/config/settings.py etc/systemd/gunicorn.service etc/systemd/gunicorn.socket
 
+echo "migrate and static"
 python core/manage.py migrate
-python core/manage.py collectstatic
+python core/manage.py collectstatic -y
 
-sudo ln -s $project_path/etc/nginx/site_dc.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/etc/systemd/gunicorn.service /etc/systemd/system/
-sudo ln -s $project_path/etc/systemd/gunicorn.socket /etc/systemd/system/
+echo "Копирование настроек Nginx и Gunicorn"
+sudo cp -f etc/nginx/my_site.conf etc/nginx/$project_domain.conf
+sudo cp -f etc/systemd/gunicorn.service etc/systemd/$project_domain.service
+sudo cp -f etc/systemd/gunicorn.socket etc/systemd/$project_domain.socket
 
+echo "Подключение настроек Nginx и Gunicorn"
+sudo ln -s $project_path/etc/nginx/$project_domain.conf /etc/nginx/sites-enabled/
+sudo ln -s $project_path/etc/systemd/$project_domain.service /etc/systemd/system/
+sudo ln -s $project_path/etc/systemd/$project_domain.socket /etc/systemd/system/
 
+echo "Перезагрузка Nginx и Gunicorn"
 sudo systemctl daemon-reload
-sudo systemctl enable gunicorn
-sudo systemctl start gunicorn
+sudo systemctl enable $project_domain
+sudo systemctl start $project_domain
 sudo service nginx reload
-sudo systemctl daemon-reload
-sudo systemctl restart gunicorn
+sudosudo systemctl restart $project_domain
--- install_ssl_certbot_only_one.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-project_domain=inkov.online
-
-sudo certbot --nginx -d $project_domain
-sudo certbot --nginx -d dev-test.$project_domain
