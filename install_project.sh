11dbaf843016336ed21b40ab7fd8a29ee0f9edba
Alexey Inkov <alex0982@mail.ru>
2024-12-14 20:57:09 +0300

    fix install_project install_project no change repo

--- install_project.sh
+++ install_project.sh
@@ -34,23 +34,21 @@ pip install -U pip
 pip install -r requirements.txt
 python -V
 
-echo "Подготовка настроек Nginx и Gunicorn"
-sed -i "s~dbms_template_path~$project_path~g" etc/nginx/my_site.conf etc/systemd/gunicorn.service
-sed -i "s~dbms_template_domain~$project_domain~g" etc/nginx/my_site.conf core/config/settings.py etc/systemd/gunicorn.service etc/systemd/gunicorn.socket
-
 echo "migrate and static"
 python core/manage.py migrate
 python core/manage.py collectstatic
 
 echo "Копирование настроек Nginx и Gunicorn"
-sudo cp -f etc/nginx/my_site.conf etc/nginx/$project_domain.conf
-sudo cp -f etc/systemd/gunicorn.service etc/systemd/$project_domain.service
-sudo cp -f etc/systemd/gunicorn.socket etc/systemd/$project_domain.socket
+sudo cp -f etc/nginx/my_site.conf /etc/nginx/sites-available/$project_domain.conf
+sudo cp -f etc/systemd/gunicorn.service /etc/systemd/system/$project_domain.service
+sudo cp -f etc/systemd/gunicorn.socket /etc/systemd/system/$project_domain.socket
 
-echo "Подключение настроек Nginx и Gunicorn"
-sudo ln -s $project_path/etc/nginx/$project_domain.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/etc/systemd/$project_domain.service /etc/systemd/system/
-sudo ln -s $project_path/etc/systemd/$project_domain.socket /etc/systemd/system/
+echo "Подготовка настроек Nginx и Gunicorn"
+sed -i "s~dbms_template_path~$project_path~g" /etc/nginx/sites-available/$project_domain.conf /etc/systemd/system/$project_domain.service
+sed -i "s~dbms_template_domain~$project_domain~g" /etc/nginx/sites-available/$project_domain.conf /etc/systemd/system/$project_domain.service /etc/systemd/system/$project_domain.socket
+
+echo "Подключение настроек Nginx"
+sudo ln -s /etc/nginx/sites-available/$project_domain.conf /etc/nginx/sites-enabled/
 
 echo "Перезагрузка Nginx и Gunicorn"
 sudo systemctl daemon-reload
