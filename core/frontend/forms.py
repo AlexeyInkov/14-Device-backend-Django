4603c079be122235aa1b1e056affe5c8b7648d61
Alexey Inkov <alex0982@mail.ru>
2024-12-20 23:05:26 +0300

    add UploadFileForm

--- core/frontend/forms.py
+++ core/frontend/forms.py
@@ -53,3 +53,25 @@ class RegisterUserForm(UserCreationForm):
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+class UploadFileForm(forms.Form):
+    file_field = MultipleFileField()
