667e0a0907305da15b3e95579d50007c6a3ca43d
Alexey Inkov <alex0982@mail.ru>
2025-01-09 22:27:39 +0300

    rename query_get_pk into get_instance_from_query_pk add convert_date_str

--- core/apps/frontend/templatetags/context_tags.py
+++ core/apps/frontend/templatetags/context_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import QuerySet
 
 from config.settings import HEADERS_ADDRESS, HEADERS_DEVICE, HEADERS_VERIFICATION
 
@@ -27,6 +28,7 @@ def dictitem(dictionary: dict, key: str) -> str:
 
 @register.filter
 def attr(obj, name):
+    # возвращает значение атрибута по имени поля
     value = getattr(obj, name, None)
     # print(f"{name} : {value}")
     if value is not None:
@@ -35,6 +37,15 @@ def attr(obj, name):
 
 
 @register.filter()
-def query_get_pk(query, pk):
-    item = query.get(pk=int(pk))
-    return item
+def get_instance_from_query_pk(query: QuerySet, pk: str):
+    # возвращает экземпляр из QuerySet по pk
+    return query.get(pk=int(pk))
+
+
+@register.filter
+def convert_date_str(date_field: str) -> str:
+    # Конвертирует YYYY-MM-DD HH:mm:ss.ms -> DD.MM.YYYY
+    if date_field is not None:
+        date_field = '2024-12-25 21:13:06.006498'
+        return '.'.join(date_field.split()[0].split('-')[-1::-1])
+    return "пусто"
