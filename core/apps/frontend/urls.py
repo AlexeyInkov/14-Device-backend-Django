507a06ff27e442fc5ff0746c2f3d9c55378e56ec
Alexey Inkov <alex0982@mail.ru>
2025-01-30 22:48:22 +0300

    add url for start celery task

--- core/apps/frontend/urls.py
+++ core/apps/frontend/urls.py
@@ -4,6 +4,7 @@ from config.settings import LOGIN_URL
 from .views import (
     IndexView,
     DeviceDetailView,
+refresh_valid_date_view
 )
 from ..my_auth.views import LoginUserView, RegisterUserView, MyLogoutView
 
@@ -13,5 +14,6 @@ urlpatterns = [
 
     path("", IndexView.as_view(), name="home"),
     path("device/<int:pk>/", DeviceDetailView.as_view(), name="detail_device"),
+    path("refresh-valid-date/", refresh_valid_date_view, name="refresh_valid_date"),
 
 ]
