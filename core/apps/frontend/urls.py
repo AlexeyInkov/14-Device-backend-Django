099b6cd2adb1396511432d12b52d88bf13557ca0
Alexey Inkov <alex0982@mail.ru>
2025-02-07 00:01:03 +0300

    add update_device_verification url

--- core/apps/frontend/urls.py
+++ core/apps/frontend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import IndexView, DeviceDetailView, refresh_valid_date_view
+from .views import IndexView, DeviceDetailView, refresh_valid_date_view, device_verifications_update_view
 
 app_name = "frontend"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="home"),
     path("device/<int:pk>/", DeviceDetailView.as_view(), name="detail_device"),
+    path("device-verifications/update/<int:pk>/", device_verifications_update_view, name="update_device_verification"),
     path("refresh-valid-date/", refresh_valid_date_view, name="refresh_valid_date"),
 ]
