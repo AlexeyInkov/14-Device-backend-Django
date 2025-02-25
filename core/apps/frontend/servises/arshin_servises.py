0670f02f1fea7e3b2c6854d7bc75e24c17889428
Alexey Inkov <alex0982@mail.ru>
2025-02-25 20:07:20 +0300

    add timeout request to arshin

--- core/apps/frontend/servises/arshin_servises.py
+++ core/apps/frontend/servises/arshin_servises.py
@@ -10,5 +10,5 @@ def request_to_arshin(reg_number: str, number: str) -> Response:
     url = f"https://fgis.gost.ru/fundmetrology/eapi/vri/?mit_number={reg_number}*&mi_number={number}&verification_date_start=2010-01-01"
 
     sleep(settings.TIME_DDOS_FOR_REQUEST)
-    response = request("get", url)
+    response = request("get", url, timeout=3)
     return response
