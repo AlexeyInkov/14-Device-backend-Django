d3e040dd44c53bc57bba58421ce644160a45867a
Alexey Inkov <alex0982@mail.ru>
2025-03-06 01:59:33 +0300

    fix template

--- core/apps/frontend/static/frontend/js/dialog.js
+++ core/apps/frontend/static/frontend/js/dialog.js
@@ -3,22 +3,33 @@ document.body.addEventListener('htmx:configRequest', (event) => {
     event.detail.headers['X-CSRFToken'] = CSRF_TOKEN;
 })
 
-// https://blog.benoitblanchon.fr/django-htmx-modal-form/
+// // https://blog.benoitblanchon.fr/django-htmx-modal-form/
+//
+// const verificationsModal = new bootstrap.Modal(document.getElementById("verificationsModal"))
+// const loadFileModal = new bootstrap.Modal(document.getElementById("loadFileModal"))
+// console.log(verificationsModal)
+// console.log(loadFileModal)
+// htmx.on("htmx:afterSwap", (e) => {
+//   console.log(e.detail)
+//   // Response targeting #dialog => show the modal
+//   if (e.detail.target.id == "dialogVerifications") {
+//     verificationsModal.show()
+//   }
+//   if (e.detail.target.id == "dialogLoadFile") {
+//       loadFileModal.show()
+//     }
+// })
+//
+// htmx.on("htmx:beforeSwap", (e) => {
+//   // Empty response targeting #dialog => hide the modal
+//   if (e.detail.target.id == "dialogVerifications" && !e.detail.xhr.response) {
+//     verificationsModal.hide()
+//     e.detail.shouldSwap = false
+//   }
+//   if (e.detail.target.id == "dialogLoadFile" && !e.detail.xhr.response) {
+//     loadFileModal.hide()
+//     e.detail.shouldSwap = false
+//   }
+// })
 
-const modal = new bootstrap.Modal(document.getElementById("verificationsModal"))
-
-htmx.on("htmx:afterSwap", (e) => {
-  // Response targeting #dialog => show the modal
-  if (e.detail.target.id == "dialog") {
-    modal.show()
-  }
-})
-
-htmx.on("htmx:beforeSwap", (e) => {
-  // Empty response targeting #dialog => hide the modal
-  if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
-    modal.hide()
-    e.detail.shouldSwap = false
-  }
-})
 
