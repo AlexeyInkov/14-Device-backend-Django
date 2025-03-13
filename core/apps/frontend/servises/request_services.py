cf7e979a5a181ff3a715ca195facafd7720b5492
Alexey Inkov <alex0982@mail.ru>
2025-03-13 21:56:43 +0300

    used services

--- core/apps/frontend/servises/request_services.py
+++ core/apps/frontend/servises/request_services.py
@@ -1,14 +1,19 @@
+import logging
+
 from django.http import HttpRequest
 
+logger = logging.getLogger(__name__)
 
-def get_org_selected(request: HttpRequest) -> str | None:
+
+def get_org_selected(request: HttpRequest) -> str:
     org_selected_request = request.GET.get("organization")
     org_selected = request.session.get("org_selected")
+    logger.debug(f"org_selected_request: {org_selected_request}")
+    logger.debug(f"org_selected_session: {org_selected}")
     if org_selected_request != org_selected:
-        reset_tso_selected(request)
-        reset_cust_selected(request)
-        reset_mu_selected(request)
-
+        reset_selected_param(request, 'tso_selected')
+        reset_selected_param(request, 'cust_selected')
+        reset_selected_param(request, 'mu_selected')
         request.session["org_selected"] = org_selected_request
         return org_selected_request
     return org_selected
@@ -17,10 +22,11 @@ def get_org_selected(request: HttpRequest) -> str | None:
 def get_tso_selected(request: HttpRequest) -> str:
     tso_selected_request = request.GET.get("tso")
     tso_selected = request.session.get("tso_selected")
-    if tso_selected_request != tso_selected and tso_selected_request is not None:
-        reset_cust_selected(request)
-        reset_mu_selected(request)
-
+    logger.debug(f"tso_selected_request: {tso_selected_request}")
+    logger.debug(f"tso_selected_session: {tso_selected}")
+    if tso_selected_request != tso_selected:
+        reset_selected_param(request, 'cust_selected')
+        reset_selected_param(request, 'mu_selected')
         request.session["tso_selected"] = tso_selected_request
         return tso_selected_request
     return tso_selected
@@ -29,8 +35,10 @@ def get_tso_selected(request: HttpRequest) -> str:
 def get_cust_selected(request: HttpRequest) -> str:
     cust_selected_request = request.GET.get("customer")
     cust_selected = request.session.get("cust_selected")
+    logger.debug(f"cust_selected_request: {cust_selected_request}")
+    logger.debug(f"cust_selected_session: {cust_selected}")
     if cust_selected_request != cust_selected and cust_selected_request is not None:
-        reset_mu_selected(request)
+        reset_selected_param(request, 'mu_selected')
 
         request.session["cust_selected"] = cust_selected_request
         return cust_selected_request
@@ -40,23 +48,15 @@ def get_cust_selected(request: HttpRequest) -> str:
 def get_mu_selected(request: HttpRequest) -> int:
     mu_selected_request = request.GET.get("metering_unit")
     mu_selected = request.session.get("mu_selected")
+    logger.debug(f"mu_selected_request: {mu_selected_request}")
+    logger.debug(f"mu_selected_session: {mu_selected}")
     if mu_selected_request is not None:
         request.session["mu_selected"] = mu_selected_request
         return mu_selected_request
     return mu_selected
 
 
-def reset_org_selected(request: HttpRequest) -> None:
-    request.session["org_selected"] = None
-
-
-def reset_tso_selected(request: HttpRequest) -> None:
-    request.session["tso_selected"] = None
-
-
-def reset_cust_selected(request: HttpRequest) -> None:
-    request.session["cust_selected"] = None
-
-
-def reset_mu_selected(request: HttpRequest) -> None:
-    request.session["mu_selected"] = None
+def reset_selected_param(request: HttpRequest, param_name: str) -> None:
+    param = request.session.pop("param_name", None)
+    logger.debug(f"reset {param_name} = {param}")
+    logger.debug(f"org_selected_session: {request.session.get('org_selected')}")
