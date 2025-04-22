a61a651712e13ab973c55dde8453d52784623278
Alexey Inkov <alex0982@mail.ru>
2025-04-23 00:00:54 +0300

    add Profile

---
+++ core/apps/my_auth/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    telegram_id = models.PositiveIntegerField(null=True, blank=True)
+
+    @property
+    def full_name(self) -> str:
+        if self.user.first_name and self.user.last_name:
+            return f"{self.user.first_name} {self.user.last_name}"
+        return self.user.first_name or self.user.last_name
+
+    def __str__(self) -> str:
+        return str(self.user)
+
+
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
