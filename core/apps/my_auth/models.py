b0dfc5fc738030dc99de400c967979fa4bf22b62
Alexey Inkov <alex0982@mail.ru>
2025-04-23 21:21:07 +0300

    add signal create Profile

--- core/apps/my_auth/models.py
+++ core/apps/my_auth/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from social_django.models import UserSocialAuth
 
 
 class Profile(models.Model):
@@ -24,3 +25,7 @@ class Profile(models.Model):
 # def create_user_profile(sender, instance, created, **kwargs):
 #     if created:
 #         Profile.objects.create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
